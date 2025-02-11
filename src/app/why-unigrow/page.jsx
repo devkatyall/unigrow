c63c0292112b2f1b1e24e4b064e190978b22b1ac
Dev <devkatyal@protonmail.com>
2025-02-10 21:36:19 -0500

    added call to action component

--- src/app/why-unigrow/page.jsx
+++ src/app/why-unigrow/page.jsx
@@ -1,11 +1,14 @@
+import CallToAction from "@/components/Common/CallToAction";
 import YouTubePlayer from "@/components/YoutubeVideo";
 import HeroVideoDialog from "@/components/hero-video-dialog";
+import { Button } from "@/components/ui/button";
 import AllinOne from "@/components/why-us/AllinOne";
 import Comparison from "@/components/why-us/Comparison";
 import Hero from "@/components/why-us/Hero";
 import PartA from "@/components/why-us/PartA";
 import VideoSec from "@/components/why-us/VideoSec";
 import { allInOne } from "@/data";
+import Link from "next/link";
 
 export default function page() {
   return (
@@ -24,6 +27,27 @@ export default function page() {
           </p>
         </div>
       </AllinOne>
+      <CallToAction>
+        <div
+          className=" flex flex-col
+        gap-2  py-[5vh]"
+        >
+          <h2 className=" text-3xl md:text-5xl font-medium drop-shadow-lg">
+            {" "}
+            Ready to Build a Website Without the Headaches?
+          </h2>
+          <p className=" text-sm lg:text-lg text-slate-400">
+            Unigrow takes care of everything so you can focus on growing your
+            business. All-inclusive pricing. No hidden fees. Stress-free, fully
+            managed process
+          </p>
+          <div className="mt-4">
+            <Button>
+              <Link href="/book-free-consultation">Book a Free Call</Link>
+            </Button>
+          </div>
+        </div>
+      </CallToAction>
     </main>
   );
 }
--- src/components/Common/CallToAction.jsx
+++ src/components/Common/CallToAction.jsx
@@ -1,1 +1,13 @@
-rfc;
+import React from "react";
+import Aurora from "../Aurora";
+
+export default function CallToAction({ children }) {
+  return (
+    <div className="relative overflow-hidden px-[6vw] py-[2vh]">
+      <div className="absolute inset-0 h-full w-full -z-10 rotate-180">
+        <Aurora speed={0.5} />
+      </div>
+      <div className="mx-auto w-[87vw]">{children}</div>
+    </div>
+  );
+}
