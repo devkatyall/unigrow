9aa3331cda7c24e65491abed32c66e3c61423104
Dev <devkatyal@protonmail.com>
2025-02-12 03:35:45 -0500

    page transition changed

--- src/app/template.js
+++ src/app/template.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { animatePageIn } from "@/lib/animations";
+import Hamester from "@/components/Hamster";
 
 export default function Template({ children }) {
   React.useEffect(() => {
@@ -12,9 +13,14 @@ export default function Template({ children }) {
     <div>
       <div
         id="banner-1"
-        className="min-h-screen bg-black z-[100] fixed top-0 left-0 w-1/4 "
-      />
-      <div
+        className="min-h-screen bg-[#35BCF1] z-[100] fixed top-0 left-0 min-w-full flex flex-col items-center justify-center"
+      >
+        {/* <Hamester />
+        <h2 className="text-2xl md:text-4xl text-white mt-2">
+          Hamester is leading the way...
+        </h2> */}
+      </div>
+      {/* <div
         id="banner-2"
         className="min-h-screen bg-black z-[100] fixed top-0 w-1/4 left-1/4 "
       />
@@ -25,7 +31,7 @@ export default function Template({ children }) {
       <div
         id="banner-4"
         className="min-h-screen bg-black z-[100] fixed top-0 w-1/4 left-3/4 "
-      />
+      /> */}
       {children}
     </div>
   );
--- src/lib/animations.js
+++ src/lib/animations.js
@@ -5,92 +5,107 @@ import { animate } from "framer-motion";
 
 export const animatePageIn = () => {
   const bannerOne = document.getElementById("banner-1");
-  const bannerTwo = document.getElementById("banner-2");
-  const bannerThree = document.getElementById("banner-3");
-  const bannerFour = document.getElementById("banner-4");
+  // const bannerTwo = document.getElementById("banner-2");
+  // const bannerThree = document.getElementById("banner-3");
+  // const bannerFour = document.getElementById("banner-4");
 
-  if (bannerOne && bannerTwo && bannerThree && bannerFour) {
-    [bannerOne, bannerTwo, bannerThree, bannerFour].forEach(
-      (banner) => (banner.style.transform = "translateY(0%)")
-    );
+  // if (bannerOne && bannerTwo && bannerThree && bannerFour) {
+  //   [bannerOne, bannerTwo, bannerThree, bannerFour].forEach(
+  //     (banner) => (banner.style.transform = "translateY(0%)")
+  //   );
+
+  if (bannerOne) {
+    bannerOne.style.transform = "translateY(0%)";
+    bannerOne.style.borderRadius = "0";
 
     animate(0, 100, {
-      duration: 0.3,
+      duration: 1,
       onUpdate: (latest) => {
         bannerOne.style.transform = `translateY(${latest}%)`;
+        bannerOne.style.borderRadius = `${latest}%`;
+        // bannerOne.style.opacity = 1 - latest / 100;
       },
     });
 
-    animate(0, 100, {
-      duration: 0.3,
-      delay: 0.2,
-      onUpdate: (latest) => {
-        bannerTwo.style.transform = `translateY(${latest}%)`;
-      },
-    });
+    // animate(0, 100, {
+    //   duration: 0.3,
+    //   delay: 0.2,
+    //   onUpdate: (latest) => {
+    //     bannerTwo.style.transform = `translateY(${latest}%)`;
+    //   },
+    // });
 
-    animate(0, 100, {
-      duration: 0.3,
-      delay: 0.4,
-      onUpdate: (latest) => {
-        bannerThree.style.transform = `translateY(${latest}%)`;
-      },
-    });
+    // animate(0, 100, {
+    //   duration: 0.3,
+    //   delay: 0.4,
+    //   onUpdate: (latest) => {
+    //     bannerThree.style.transform = `translateY(${latest}%)`;
+    //   },
+    // });
 
-    animate(0, 100, {
-      duration: 0.3,
-      delay: 0.6,
-      onUpdate: (latest) => {
-        bannerFour.style.transform = `translateY(${latest}%)`;
-      },
-    });
+    // animate(0, 100, {
+    //   duration: 0.3,
+    //   delay: 0.6,
+    //   onUpdate: (latest) => {
+    //     bannerFour.style.transform = `translateY(${latest}%)`;
+    //   },
+    // });
   }
 };
 
 export const animatePageOut = (href, router) => {
   const bannerOne = document.getElementById("banner-1");
-  const bannerTwo = document.getElementById("banner-2");
-  const bannerThree = document.getElementById("banner-3");
-  const bannerFour = document.getElementById("banner-4");
+  // const bannerTwo = document.getElementById("banner-2");
+  // const bannerThree = document.getElementById("banner-3");
+  // const bannerFour = document.getElementById("banner-4");
 
-  if (bannerOne && bannerTwo && bannerThree && bannerFour) {
-    [bannerOne, bannerTwo, bannerThree, bannerFour].forEach(
-      (banner) => (banner.style.transform = "translateY(-100%)")
-    );
+  // if (bannerOne) {
+  //   [bannerOne, bannerTwo, bannerThree, bannerFour].forEach(
+  //     (banner) => (banner.style.transform = "translateY(-100%)")
+  //   );
+
+  if (bannerOne) {
+    bannerOne.style.transform = "translateY(-100%)";
+    bannerOne.style.borderRadius = "100%";
+    // bannerOne.style.opacity = 0;
 
     animate(-100, 0, {
-      duration: 0.5,
+      duration: 1,
       onUpdate: (latest) => {
         bannerOne.style.transform = `translateY(${latest}%)`;
-      },
-    });
-
-    animate(-100, 0, {
-      duration: 0.5,
-      delay: 0.2,
-      onUpdate: (latest) => {
-        bannerTwo.style.transform = `translateY(${latest}%)`;
-      },
-    });
-
-    animate(-100, 0, {
-      duration: 0.5,
-      delay: 0.4,
-      onUpdate: (latest) => {
-        bannerThree.style.transform = `translateY(${latest}%)`;
-      },
-    });
-
-    animate(-100, 0, {
-      duration: 0.5,
-      delay: 0.6,
-      onUpdate: (latest) => {
-        bannerFour.style.transform = `translateY(${latest}%)`;
+        bannerOne.style.borderRadius = `${-latest}%`;
+        // bannerOne.style.opacity = (latest + 100) / 100;
       },
 
       onComplete: () => {
         router.push(href);
       },
     });
+
+    // animate(-100, 0, {
+    //   duration: 0.5,
+    //   delay: 0.2,
+    //   onUpdate: (latest) => {
+    //     bannerTwo.style.transform = `translateY(${latest}%)`;
+    //   },
+    // });
+
+    // animate(-100, 0, {
+    //   duration: 0.5,
+    //   delay: 0.4,
+    //   onUpdate: (latest) => {
+    //     bannerThree.style.transform = `translateY(${latest}%)`;
+    //   },
+    // });
+
+    // animate(-100, 0, {
+    //   duration: 0.5,
+    //   delay: 0.6,
+    //   onUpdate: (latest) => {
+    //     bannerFour.style.transform = `translateY(${latest}%)`;
+    //   },
+
+    //
+    // });
   }
 };
