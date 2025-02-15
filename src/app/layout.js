e82dd45e9bda09ad2633f7b40adc2c4d16f02b29
Dev <devkatyal@protonmail.com>
2025-02-15 04:57:54 -0500

    google tag added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,23 +14,31 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-8075XGQXG3"
-          strategy="afterInteractive"
-          async
-        />
-        {/* Initialize GA */}
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="gtm-script" strategy="beforeInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-8075XGQXG3');
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f = d.getElementsByTagName(s)[0],
+                  j = d.createElement(s),
+                  dl = l!='dataLayer' ? '&l='+l : '';
+              j.async = true;
+              j.src = 'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j, f);
+            })(window,document,'script','dataLayer','GTM-5TG6KZHK');
           `}
         </Script>
       </head>
       <AppProvider>
         <body className="dark mt-[22vh]">
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5TG6KZHK"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <Transition />
           <Header />
           {children}
--- src/app/services/website-development/page.js
+++ src/app/services/website-development/page.js
@@ -13,6 +13,7 @@ import { content } from "@/webDev";
 import { BlurFade } from "@/components/magicui/blur-fade";
 import Image from "next/image";
 import image from "@/../public/services/webdev.png";
+import { cn } from "@/lib/utils";
 
 export const metadata = {
   title: "Custom Web Development Services in Canada | Unigrow",
@@ -58,7 +59,7 @@ export default function page() {
     <>
       <main className=" space-y-[10vh]">
         <section>
-          <Main className="lg:gap-[2vh]">
+          <Main className="lg:gap-[2vh] pt-[4vh]">
             <MainFlag>Web Development Services in Canada</MainFlag>
             <MainHeading className={"lg:w-[90%]"}>
               High-Performance, SEO-Optimized Websites for Businesses & Startups
@@ -97,7 +98,7 @@ export default function page() {
               {content.why_choose_us.benefits.map((e, i) => (
                 <div
                   key={i}
-                  className=" drop-shadow-[0px_0px_10px_#3D3C3B] ring-1 ring-cyan-600/20 md:h-[23vh] flex flex-col items-center justify-center lg:h-[30vh] xl:h-[25vh] p-3 rounded-xl w-[calc(100%)] md:w-[calc(100%/3)] lg:w-[calc(100%/4)] bg-neutral-800"
+                  className=" drop-shadow-[0px_0px_10px_#3D3C3B] ring-1 ring-cyan-600/20 md:h-[23vh] flex flex-col items-center justify-center lg:h-[30vh] xl:h-[25vh] p-3 rounded-lg w-[calc(100%)] md:w-[calc(100%/3)] lg:w-[calc(100%/4)] bg-neutral-800"
                 >
                   <BlurFade inView delay={0.25 * i}>
                     <span className=" text-4xl my-[1vh] block">{e.icon}</span>
@@ -106,6 +107,49 @@ export default function page() {
                     </h4>
                     <SubText>{e.description}</SubText>
                   </BlurFade>
+                </div>
+              ))}
+            </div>
+          </Sub>
+        </section>
+        <section className="">
+          <Sub>
+            <SubHeading>{content.services.title}</SubHeading>
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 grid-rows-auto gap-4 lg:gap-[2vh] py-[2vh]">
+              {content.services.items.map((e, i) => (
+                <div
+                  key={i}
+                  className={cn(
+                    "p-6 ring-1 ring-white/30 flex flex-col gap-1 h-full rounded-lg",
+                    i % 2 === 0 && "lg:row-span-2",
+                    i === 5 && ""
+                  )}
+                >
+                  <h3 className="text-xl md:text-2xl my-[1vh]">{e.title}</h3>
+                  <SubText className={"mt-0"}>{e.description}</SubText>
+                  <div>
+                    {e.features.map((f, j) => (
+                      <div key={j} className="flex items-start gap-2">
+                        <span className="text-sm md:text-base  " key={j}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-5 drop-shadow-[0px_0px_10px_#3D3C3B] fill-white/50"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                            />
+                          </svg>
+                        </span>
+                        <p className="text-white/50">{f}</p>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               ))}
             </div>
